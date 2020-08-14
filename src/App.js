cf447eefa2b1c8eef5d913ba0cabdf625b82d414
Charutha <charuthabandara@gmail.com>
2020-08-14 00:39:49 -0400

    setup reducers and global state

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import { Header } from './components/Header';
 import { Balance } from './components/Balance';
 import { IncomeExpenses } from './components/IncomeExpenses';
 import { TransactionList } from './components/TransactionList';
+import { AddTransaction } from './components/AddTransaction';
+import { GlobalProvider } from './context/GlobalState';
 
 import './App.css';
-import { AddTransaction } from './components/AddTransaction';
+
+
 
 function App() {
   return (
-    <div>
+    <GlobalProvider>
       <Header />
       <div className="container">
         <Balance />
@@ -17,7 +20,7 @@ function App() {
         <TransactionList />
         <AddTransaction />
       </div>
-    </div>
+    </GlobalProvider>
   );
 }
 
